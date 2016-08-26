26c275a47aa3ea43cab1d3725cc21fc4d046be64
Jack Lawson ajacksified@gmail.com <ajacksified@gmail.com>
2016-08-25 22:22:12 -0700

    Export ChatPlugin properly; v2.0.2

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,8 +2,7 @@ export { default as Plugin } from './plugin';
 export { default as EventPlugin } from './event';
 export { default as HTTPPlugin } from './http';
 
-export { default as ChatPlugin } from './chat';
-export { respond, listen, help, permissionGroup } from './chat';
+export { ChatPlugin, respond, listen, help, permissionGroup } from './chat';
 
 import * as p from './plugins';
 export const plugins = p;
