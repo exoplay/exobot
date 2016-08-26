38111cc5254fbb13260e0f5c7ba04188504117e5
Jack Lawson ajacksified@gmail.com <ajacksified@gmail.com>
2016-08-25 22:07:56 -0700

    Export plugin decorators

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,9 @@
 export { default as Plugin } from './plugin';
-export { default as ChatPlugin } from './chat';
 export { default as EventPlugin } from './event';
 export { default as HTTPPlugin } from './http';
+
+export { default as ChatPlugin } from './chat';
+export { respond, listen, help, permissionGroup } from './chat';
 
 import * as p from './plugins';
 export const plugins = p;
