734c3d2e2d7728ea327347e736f9e0c44dd4aaea
Jack Lawson <ajacksified@gmail.com>
2016-12-20 11:01:18 -0800

    uptime command

---
+++ src/plugins/plugins/uptime.js
@@ -0,0 +1,42 @@
+import { ChatPlugin, respond, help, permissionGroup } from '../chat';
+
+const start = new Date();
+
+const SECONDS = 1000;
+const MINUTES = SECONDS * 60;
+const HOURS = MINUTES * 60;
+const DAYS = HOURS * 24
+
+export class Uptime extends ChatPlugin {
+  static _name = 'uptime';
+  static propTypes = {};
+
+  @help('/uptime shows time since last restart.');
+  @permissionGroup('uptime');
+  @respond(/^uptime/i);
+  pluginUptime () {
+    const now = new Date();
+    let diff = now - start;
+
+    const days = parseInt(diff / DAYS, 10);
+    diff = diff - (days * DAYS);
+
+    const hours = parseInt(diff / HOURS, 10);
+    diff = diff - (hours * HOURS);
+
+    const minutes = parseInt(diff / MINUTES, 10);
+    diff = diff - (minutes * MINUTES);
+
+    const seconds = parseInt(diff / SECONDS, 10);
+    diff = diff - (seconds * SECONDS);
+
+    const message = [];
+
+    if (days) { message.push(`${days} days`) ; }
+    if (hours) { message.push(`${hours} hours`) ; }
+    if (minutes) { message.push(`${minutes} minutes`) ; }
+    if (seconds) { message.push(`${seconds} seconds`) ; }
+
+    return message.join(',');
+  }
+}
